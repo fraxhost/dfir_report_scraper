353a883914fc5aa13920f790ba081d0a940c4fd4
Ahmed Ryan <ahmedryanfaiyaz@gmail.com>
2025-02-25 15:24:39 +0600

    feature: fetch titles from all pages;

--- dfir_report_scraper/spiders/dfir_report_spider.py
+++ dfir_report_scraper/spiders/dfir_report_spider.py
@@ -7,6 +7,13 @@ class DfirReportSpiderSpider(scrapy.Spider):
     start_urls = ["https://thedfirreport.com"]
 
     def parse(self, response):
-        
-        yield response
-        
+
+        titles = response.css("h2.entry-title a::text").getall()
+
+        for title in titles:
+            yield {"title": title}
+
+        next_page = response.css("div.nav-links a.next.page-numbers::attr(href)").get()
+
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
